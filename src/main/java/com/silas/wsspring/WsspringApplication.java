0490f41bf589b20916d8a6c2ac4ee34f975ae5fb
Silas Ferreira <silasferreira96@hotmail.com>
2018-06-01 17:02:48 -0300

    Projeto criado

---
+++ src/main/java/com/silas/wsspring/WsspringApplication.java
@@ -0,0 +1,12 @@
+package com.silas.wsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsspringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/silas/wsspring/WsspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.silas.wsspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
