87de172db64ab45db583a57a3e34cfce2ce2bc2d
Silas Ferreira <silasferreira96@hotmail.com>
2018-06-01 18:18:51 -0300

    Banco de dados h2 e a criação automatica da base de dados

--- src/main/java/com/silas/wsspring/resources/CategoriaResource.java
+++ src/main/java/com/silas/wsspring/resources/CategoriaResource.java
@@ -3,7 +3,6 @@ package com.silas.wsspring.resources;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.internal.Arrays;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
