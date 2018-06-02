c8e6a6fa07d4538741205a4d4cf9ec48331c9e00
Silas Ferreira <silasferreira96@hotmail.com>
2018-06-02 09:17:46 -0300

    Criando operacao de instanciacao

--- src/main/java/com/silas/wsspring/WsspringApplication.java
+++ src/main/java/com/silas/wsspring/WsspringApplication.java
@@ -1,12 +1,31 @@
 package com.silas.wsspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WsspringApplication {
+import com.silas.wsspring.domain.Categoria;
+import com.silas.wsspring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WsspringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WsspringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
