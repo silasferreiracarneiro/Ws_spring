33dd00bb3e3fa6dd0aa38a30034559618b6e735c
Silas Ferreira <silasferreira96@hotmail.com>
2018-06-02 10:11:29 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/silas/wsspring/resources/exception/StandardError.java
@@ -0,0 +1,44 @@
+package com.silas.wsspring.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long timeSpan;
+	
+	public StandardError(Integer status, String msg, Long timeSpan) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeSpan = timeSpan;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTimeSpan() {
+		return timeSpan;
+	}
+
+	public void setTimeSpan(Long timeSpan) {
+		this.timeSpan = timeSpan;
+	}
+	
+	
+}
